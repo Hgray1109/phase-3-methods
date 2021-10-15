db3be0ccbcd27e4cc81b9a4396a3cfc3ab6d7da3
Hashem Gray <Hgray1109@gmail.com>
2021-10-15 12:01:27 -0400

    commit tree 399c0b85001ac348587c0fa1bcad2c67737adc20

--- methods.rb
+++ methods.rb
@@ -1,1 +1,51 @@
 # Your code here!
+
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
+
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet("Naureen")
+    puts "Hello, #{name}"
+end
+
+def greet_with_default(name)
+    name = "programmer"
+    puts "Hello, #{name}"
+end
+
+def add num1, num2
+    num1 + num2
+    add 1, 2
+end
+
+def halve num1
+    if num1 != integer
+        return nil
+    end
+
+    num1/2
+end
